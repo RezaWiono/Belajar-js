38f137201c6325bac0ad76382404c70dd7dff588
reza <reza.wiono1900@gmail.com>
2019-01-16 10:51:36 +0700

    javascript

--- index.js
+++ index.js
@@ -46,13 +46,59 @@
 // console.log(selectionColor.length)
 
 //#function & types of function
-function greet(Name , lastName) {
-    console.log('hello '+ Name + ' ' + lastName);
-}
-//calculating a value
-function square(number) {
-    return number * number;
-}
-// greet('reza', 'wiono');
+// function greet(Name , lastName) {
+//     console.log('hello '+ Name + ' ' + lastName);
+// }
+// //calculating a value
+// function square(number) {
+//     return number * number;
+// }
+// // greet('reza', 'wiono');
 
-console.log(square(2))
+// console.log(square(2))
+
+//#Arithmatic Oprators
+// let x = 10;
+// let y =3;
+// // console.log(x+y);
+// // console.log(x-y);
+// // console.log(x*y);
+// // console.log(x/y);
+// // console.log(x%y);
+// // console.log(x**y);
+
+// //increment(++)
+// // console.log(x++);
+// // console.log(x);
+
+// //decrement(--)
+// console.log(--x);
+
+//#Assignment oprators
+// let x = 10;
+
+// x = x + 5;
+// x += 5;
+
+// x = x * 3;
+// x *= 3;
+
+// //#Comparison oprators
+// let x = 1;
+// //relationnal
+// console.log(x > 0);
+// console.log(x >= 1);
+// console.log(x < 1);
+// console.log(x <= 1);
+// //equality
+// console.log(x === 1);
+// console.log(x !== 1);
+
+
+//#Equality oprators
+console.log(1 === 1);
+console.log('1' === 1);
+
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
