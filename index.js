e587ccf8304bcbec643a8142719f868c70225a40
reza <reza.wiono1900@gmail.com>
2019-01-16 11:58:37 +0700

    javascript

--- index.js
+++ index.js
@@ -40,12 +40,12 @@
 
 // console.log(person);
 
-// #Array
+// #6. Array
 // let selectionColor = ['red', 'blue'];
 // selectionColor[2] = 1;
 // console.log(selectionColor.length)
 
-//#function & types of function
+//#7&8. function & types of function
 // function greet(Name , lastName) {
 //     console.log('hello '+ Name + ' ' + lastName);
 // }
@@ -57,7 +57,7 @@
 
 // console.log(square(2))
 
-//#Arithmatic Oprators
+//#2. Arithmatic Oprators
 // let x = 10;
 // let y =3;
 // // console.log(x+y);
@@ -74,7 +74,7 @@
 // //decrement(--)
 // console.log(--x);
 
-//#Assignment oprators
+//#3. Assignment oprators
 // let x = 10;
 
 // x = x + 5;
@@ -83,7 +83,7 @@
 // x = x * 3;
 // x *= 3;
 
-// //#Comparison oprators
+// //#4. Comparison oprators
 // let x = 1;
 // //relationnal
 // console.log(x > 0);
@@ -95,10 +95,54 @@
 // console.log(x !== 1);
 
 
-//#Equality oprators
-console.log(1 === 1);
-console.log('1' === 1);
+// //#5. Equality oprators
+// console.log(1 === 1);
+// console.log('1' === 1);
 
-console.log(1 == 1);
-console.log('1' == 1);
-console.log(true == 1);
+// console.log(1 == 1);
+// console.log('1' == 1);
+// console.log(true == 1);
+
+
+// //#6. Ternary operator
+// let point = 80;
+// let type = point > 100 ? 'gold': 'silver';
+
+// console.log(type);
+
+
+// //#7. logical operatoers
+// let highIncome = false;
+// let goodCreditScore = false;
+// let eligibleForLoan = highIncome  goodCreditScore;
+// console.log('Eligible', eligibleForLoan);
+
+// let applicationRefused = !eligibleForLoan;
+// Console.log('Application Refused' applicationRefused);
+
+
+// //#8. logical operation with npn-booleans
+// let userColor = undefined;
+// let defaulyColor = 'blue';
+// let currentColor = userColor && defaulyColor;
+
+// console.log(currentColor);
+
+
+// //#9. Bitwise operators
+// const readPermission = 4;
+// const writePermission = 2;
+// const  executePermission =1;
+
+// let myPermission = 0;
+// myPermission = myPermission / readPermission / writePermission;
+
+// let message =
+//     (myPermission & ReadableStream) ? 'yes': 'no';
+
+// console.log(messege);
+
+
+// //#10. operators precedence
+let x = (2 + 3) * 4;
+console.log(x);
