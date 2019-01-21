be30d1997da4c5ac273d04c4e4df829d42312128
reza <reza.wiono1900@gmail.com>
2019-01-21 11:13:40 +0700

    javascript

--- index.js
+++ index.js
@@ -256,18 +256,18 @@
 
 
 // //20. exercis-prime numbers
-showPrime(10);
+// showPrime(10);
 
-function showPrime(limit) {
-    for (let number = 2; number <= limit; number++)
-        if (isPrime(number)) console,log(number);
-            }    
+// function showPrime(limit) {
+//     for (let number = 2; number <= limit; number++)
+//         if (isPrime(number)) console,log(number);
+//             }    
 
-function isPrime(number){
-    for (let factor = 2; factor < number; factor++);
-    if (number % factor === 0)
-    return false;
+// function isPrime(number){
+//     for (let factor = 2; factor < number; factor++);
+//     if (number % factor === 0)
+//     return false;
 
-    return true;
+//     return true;
 
-}
+// }
